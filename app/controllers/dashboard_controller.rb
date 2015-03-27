e5f8079f575baf7d1aebec1914864dd1a6796202
svirabouth <stephane.virabouth@gmail.com>
2015-03-27 16:44:46 +0100

    Update dashboard_controller.rb

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,6 +8,10 @@ class DashboardController < ApplicationController
   
   def addEvent
     gz = open('http://data.githubarchive.org/2015-01-01-15.json.gz')
+    uri    = 'mongodb://test:test@ds0ds033617.mongolab.com:33617/dashboard_project'
+  	client = MongoClient.from_uri(uri)
+  	db     = client['dashboard_project']
+  	coll = db.collection("events")
   end
 
 end
