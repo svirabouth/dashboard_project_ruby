e6dd97ae43869e3667473572445194000293e3ee
svirabouth <stephane.virabouth@gmail.com>
2015-03-27 16:52:55 +0100

    Update dashboard_controller.rb

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'mongo'
 require 'json'
+require 'zlib'
 include Mongo
 class DashboardController < ApplicationController
   def index
@@ -8,6 +9,7 @@ class DashboardController < ApplicationController
   
   def addEvent
     gz = open('http://data.githubarchive.org/2015-01-01-15.json.gz')
+    js = Zlib::GzipReader.new(gz).read
    uri    = 'mongodb://test:test@ds033617.mongolab.com:33617/dashboard_project'
 	client = MongoClient.from_uri(uri)
 	db     = client['dashboard_project']
