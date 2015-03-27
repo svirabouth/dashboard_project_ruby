f47c159291755b29e920f740080e4ded9a201705
svirabouth <stephane.virabouth@gmail.com>
2015-03-27 16:30:15 +0100

    Update dashboard_controller.rb

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -12,15 +12,11 @@ class DashboardController < ApplicationController
   	=begin
   	gz = open('http://data.githubarchive.org/2015-01-01-15.json.gz')
 	js = Zlib::GzipReader.new(gz).read
-	=end
 	uri    = 'mongodb://test:test@ds0ds033617.mongolab.com:33617/dashboard_project'
 	client = MongoClient.from_uri(uri)
 	db     = client['dashboard_project']
-	=begin
 	coll = db.collection("events")
-	
 	coll.drop()
-
 	Yajl::Parser.parse(js) do |event|
 		coll.insert(event)
 	break
